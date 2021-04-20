e2057452f8d37733ee84bd2e8a3cebc8dfa5a86e
Tales Salinas Rozo <talesrozo@hotmail.com>
2021-04-19 22:05:10 -0400

    The API is connected and working.

--- climate/views.py
+++ climate/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+#https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=424AD744-AE89-490D-9F44-51C24F70E950
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=424AD744-AE89-490D-9F44-51C24F70E950")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
